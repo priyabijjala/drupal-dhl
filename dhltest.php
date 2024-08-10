332d0fecf1eb0a33301d83752d967af68b44a85e
priyabijjala <priya.bijjala077@gmail.com>
2024-08-10 13:45:32 +0530

    Update dhltest.php

--- dhltest.php
+++ dhltest.php
@@ -7,13 +7,13 @@ class LocationFinderFormTest extends BrowserTestBase {
   protected static $modules = ['dhl_location_finder'];
 
   public function testLocationFinderForm() {
-    $this->drupalGet('/dhl-location-finder');
+    $this->drupalGet('/location-finder');
     $this->assertSession()->statusCodeEquals(200);
     $this->assertSession()->fieldExists('country');
     $this->assertSession()->fieldExists('city');
     $this->assertSession()->fieldExists('postal_code');
 
-    $this->drupalPostForm('/dhl-location-finder', [
+    $this->drupalPostForm('/location-finder', [
       'country' => 'Czechia',
       'city' => 'Prague',
       'postal_code' => '11000',
